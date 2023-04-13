d0d1e2ec6ad48e81db232617649c598f1304e076
Tony <thunter1987@yahoo.com>
2023-04-13 00:09:41 -0500

    Tests Passed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(el => el === name.toLowerCase() || el === name)
+}
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(el => el.slice(0, 2) === name.slice(0, 2))
+}
+function matchName(drivers, name) {
+    return drivers.filter(el => el.name === name)
+}
